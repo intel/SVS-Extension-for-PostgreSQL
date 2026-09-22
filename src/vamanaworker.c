d876632e2b66d968adf2e8f7929a0ce9df9bf5f1
klmckeig <kelly.l.mckeighan@intel.com>
2026-09-22 10:32:11 -0700

    fix: close eviction-suppression window before starting the reload transaction
    Signed-off-by: klmckeig <kelly.l.mckeighan@intel.com>

--- src/vamanaworker.c
+++ src/vamanaworker.c
@@ -395,16 +395,16 @@ VamanaWorkerProcessReloads(void)
 		 * (causing a reload loop).
 		 */
 		VamanaEvictCacheEntry(relid);
+		vamana_active_load_relid = relid;
 		SetCurrentStatementStartTimestamp();
 		StartTransactionCommand();
 		PushActiveSnapshot(GetTransactionSnapshot());
 
-		vamana_active_load_relid = relid;
 		(void) VamanaWorkerGetOrLoadIndex(relid, NULL, false);
-		vamana_active_load_relid = InvalidOid;
 
 		PopActiveSnapshot();
 		CommitTransactionCommand();
+		vamana_active_load_relid = InvalidOid;
 
 		anyReload = true;
 	}
